c4f2588410575936607e5471121d1f2468da7e0f
suryaescuerdo <suryaescuerdo26@gmail.comm>
2021-10-18 08:43:53 +0700

    first commitgit commit -m

---
+++ app/Controllers/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+	public function index()
+	{
+		echo view('/template/header.php');
+		echo view('/template/body.php');
+		echo view('/template/footer.php');
+	}
+}
